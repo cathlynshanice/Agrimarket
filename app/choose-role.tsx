21587edc4c3d30d5592022765cda025d3c317c6c
felissia <felity.13@gmail.com>
2025-04-29 21:12:24 +0700

    Update choose-role.tsx

--- app/choose-role.tsx
+++ app/choose-role.tsx
@@ -1,49 +1,49 @@
-import { View, Text, StyleSheet } from 'react-native';
-import RoleCard from 'Agrimarket/components/RoleCard';// adjust the path if needed  
-import { useRouter } from 'expo-router';
+// import { View, Text, StyleSheet } from 'react-native';
+// // import RoleCard from 'Agrimarket/components/RoleCard';// adjust the path if needed  
+// import { useRouter } from 'expo-router';
 
-export default function ChooseRolePage() {
-  const router = useRouter();
+// export default function ChooseRolePage() {
+//   const router = useRouter();
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.heading}>Pilih Peran</Text>
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.heading}>Pilih Peran</Text>
 
-      <RoleCard
-        title="Petani"
-        image={require('../../assets/image/farmer.png')}
-        // onPress={() => router.push('/farmer')}
-      />
-      <RoleCard
-        title="Pengelolah"
-        image={require('../../assets/image/processor.png')}
-        // onPress={() => router.push('/processor')}
-      />
-      <RoleCard
-        title="Distributor"
-        image={require('../../assets/image/distributor.png')}
-        // onPress={() => router.push('/distributor')}
-      />
-      <RoleCard
-        title="Konsumen"
-        image={require('../../assets/image/consumer.png')}
-        // onPress={() => router.push('/consumer')}
-      />
-    </View>
-  );
-}
+//       <RoleCard
+//         title="Petani"
+//         image={require('../../assets/image/farmer.png')}
+//         // onPress={() => router.push('/farmer')}
+//       />
+//       <RoleCard
+//         title="Pengelolah"
+//         image={require('../../assets/image/processor.png')}
+//         // onPress={() => router.push('/processor')}
+//       />
+//       <RoleCard
+//         title="Distributor"
+//         image={require('../../assets/image/distributor.png')}
+//         // onPress={() => router.push('/distributor')}
+//       />
+//       <RoleCard
+//         title="Konsumen"
+//         image={require('../../assets/image/consumer.png')}
+//         // onPress={() => router.push('/consumer')}
+//       />
+//     </View>
+//   );
+// }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
-    backgroundColor: '#fff',
-  },
-  heading: {
-    fontSize: 32,
-    fontWeight: 'bold',
-    color: '#84b067',
-    marginBottom: 20,
-    fontFamily: 'Sora',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     padding: 24,
+//     backgroundColor: '#fff',
+//   },
+//   heading: {
+//     fontSize: 32,
+//     fontWeight: 'bold',
+//     color: '#84b067',
+//     marginBottom: 20,
+//     fontFamily: 'Sora',
+//   },
+// });
